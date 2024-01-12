995897cb8a50a6923e076b8f809c7300e95423e2
Wout Neirynck <wout@monkey-projects.be>
2024-01-12 15:48:31 +0100

    Use clj plugin for build

--- .monkeyci/build.clj
+++ .monkeyci/build.clj
@@ -1,33 +1,3 @@
-(ns container-instance.build
-  (:require [monkey.ci.build
-             [api :as api]
-             [core :as c]
-             [shell :as s]]))
+(require '[monkey.ci.plugin.clj :as p])
 
-(defn clj [name & args]
-  "Executes script in clojure container"
-  {:name name
-   :container/image "docker.io/clojure:temurin-21-tools-deps-alpine"
-   :script ["ls -al"
-            "df -h"
-            (apply str "clojure " args)]})
-
-(def inspect
-  (s/bash "ls -al && df -h"))
-
-(def unit-test (clj "unit-test" "-X:test:junit"))
-(def jar (clj "jar" "-X:jar"))
-
-(defn deploy [ctx]
-  (-> (api/build-params ctx)
-      (select-keys ["CLOJARS_USERNAME" "CLOJARS_PASSWORD"])
-      (as-> p (assoc (clj "deploy" "-X:jar:deploy")
-                     :container/env p))))
-
-(c/defpipeline test-and-deploy
-  [inspect
-   unit-test
-   jar
-   deploy])
-
-[test-and-deploy]
+(p/library)
